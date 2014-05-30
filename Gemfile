5cd510d1ac1e1ba7bfe3ae696906228dac6c80f9
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-05-30 16:05:19 -0300

    sign replies!

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'gpgme'
 gem 'tokyocabinet'
+
+group :development, :test do
+  gem 'pry'
+end
--- lib/notary.rb
+++ lib/notary.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 require 'tokyocabinet'
+require 'gpgme'
+
+ENV['GNUPGHOME'] = File.dirname(__FILE__) + '/../gnupg'
 
 TOKYO = TokyoCabinet::HDB::new
 TOKYO.open('notary.tch', TokyoCabinet::HDB::OWRITER | TokyoCabinet::HDB::OCREAT)
 
+GPG = GPGME::Crypto.new armor: true
+
 get '/' do
-  TOKYO.get(params[:key])
+  value = TOKYO.get(params[:key])
+
+  GPG.clearsign(value).to_s if !value.nil?
 end
 
 post '/' do
