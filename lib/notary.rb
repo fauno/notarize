f3dedc484d36765eee8fab300cebd4abb2c9cea1
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-05-30 16:51:45 -0300

    import pubkeys

--- lib/notary.rb
+++ lib/notary.rb
@@ -8,6 +8,11 @@ TOKYO = TokyoCabinet::HDB::new
 TOKYO.open('notary.tch', TokyoCabinet::HDB::OWRITER | TokyoCabinet::HDB::OCREAT)
 
 GPG = GPGME::Crypto.new armor: true
+CTX = GPGME::Ctx.new armor: true, textmode: true
+
+CTX.each_key do |k|
+  puts k
+end
 
 get '/' do
   value = TOKYO.get(params[:key])
@@ -18,4 +23,8 @@ end
 post '/' do
   TOKYO.put(params[:key], params[:value])
   TOKYO.sync
+end
+
+post '/import' do
+  'OK' if GPGME::Key.import(params[:pubkey]).imported > 0
 end
