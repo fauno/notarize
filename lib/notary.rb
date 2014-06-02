615636504d6ac89e39429b63152ce95817cc18b4
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-01 21:33:50 -0300

    comments

--- lib/notary.rb
+++ lib/notary.rb
@@ -1,23 +1,31 @@
+# encoding: utf-8
+# dependencies
 require 'sinatra'
 require 'tokyocabinet'
 require 'gpgme'
 require 'json'
 
+# use a local key, see bin/genkey
 ENV['GNUPGHOME'] = File.dirname(__FILE__) + '/../gnupg'
 
-TOKYO = TokyoCabinet::TDB::new
-TOKYO.open('db/notary.tct', TokyoCabinet::TDB::OWRITER | TokyoCabinet::TDB::OCREAT)
-
+# load context and crypto for the key
 GPG = GPGME::Crypto.new armor: true
 CTX = GPGME::Ctx.new armor: true, textmode: true
 
+# create or open a tokyo cabinet table
+TOKYO = TokyoCabinet::TDB::new
+TOKYO.open('db/notary.tct', TokyoCabinet::TDB::OWRITER | TokyoCabinet::TDB::OCREAT)
+
+# print public ring
 CTX.each_key do |k|
   puts k
 end
 
+# pass ?key=the_key to pull results
 get '/' do
   value = TOKYO.get(params[:key])
 
+# if there's something, return it as json
   if !value.nil?
     value.to_json
   else
@@ -25,11 +33,18 @@ get '/' do
   end
 end
 
+# push info here
+# key: the key for the info we want to store
+# value: the value
+# sig: the gpg sig of the value
 post '/' do
+# the notary verifies the signature
   CTX.verify(GPGME::Data.new(params[:sig]), GPGME::Data.new(params[:value]))
 
+# get the signature
   sig = CTX.verify_result.signatures[0]
 
+# if the sig is valid
   if sig.valid?
 
 # values to store, we use string keys to avoid an implicit conversion
@@ -40,16 +55,20 @@ post '/' do
       'fpr' => sig.fpr
     }
 
+# store the data and return OK to the client
     if TOKYO.put(params[:key], store)
       TOKYO.sync
       'OK'
     end
+# otherwise complain
   else
     puts 'invalid or unrecognized: ' + sig.fpr
     'INVALID'
   end
 end
 
+# send a key here
+# pubkey: ascii armored key(s)
 post '/import' do
   'OK' if GPGME::Key.import(params[:pubkey]).imported > 0
 end
